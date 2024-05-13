5f38f9ada032a17876cebb035f0c86d0fb6aef1b
jakub-ner <kubaner1@gmail.com>
2024-05-13 18:24:44 +0100

    optimizations

--- ZAD2 MinMax/halmaGame/MinMax.go
+++ ZAD2 MinMax/halmaGame/MinMax.go
@@ -7,12 +7,12 @@ import (
 func MoveSelection(board *Board, currentPlayer utils.Player) (*Pawn, *Move) {
 	originalMoves, originalPawns := getMovesAndPawns(board, &currentPlayer)
 
-	tree := NewNode(nil, board, 0, currentPlayer, nil, nil)
+	tree := NewNode(nil, nil)
 	channel := make(chan struct {
 		*Node
 		int8
 	}, 1)
-	tree.selectScore(channel, 0)
+	tree.selectScore(channel, 0, board, 0, currentPlayer)
 
 	bestKid := <-channel
 	var bestKidNode *Node
@@ -22,7 +22,6 @@ func MoveSelection(board *Board, currentPlayer utils.Player) (*Pawn, *Move) {
 			break
 		}
 	}
-	//bestKidNode := tree.Children[bestKid.int8]
 	for i, originalPawn := range originalPawns {
 		if &originalPawn.Coords == bestKidNode.initialCords {
 			return originalPawn, &originalMoves[i]
--- ZAD2 MinMax/halmaGame/Node.go
+++ ZAD2 MinMax/halmaGame/Node.go
@@ -7,16 +7,10 @@ import (
 )
 
 type Node struct {
-	Children []*Node // to be calculated
-	Parent   *Node   // passed to children
-	score    int     // to be calculated
-
-	currentPlayer utils.Player
-	board         *Board                      // passed to children
-	pawn          *Pawn                       // to be calculated
-	initialCords  *Coords                     // to be calculated
-	nestingLevel  int8                        // passed to children
-	comparator    func(val int, max int) bool // to be calculated
+	Children     []*Node
+	score        int
+	pawn         *Pawn
+	initialCords *Coords
 }
 
 func getMovesAndPawns(board *Board, currentPlayer *utils.Player) ([]Move, []*Pawn) {
@@ -44,60 +38,58 @@ func isOpponent(nestingLevel int8) bool {
 	return nestingLevel%2 == 1
 }
 
-func NewNode(parent *Node, board *Board, nestingLevel int8, nextPlayer utils.Player, pawn *Pawn, initialCords *Coords) *Node {
-	comparator := findMax
-	if isOpponent(nestingLevel) {
-		comparator = findMin
-	}
-
+func NewNode(pawn *Pawn, initialCords *Coords) *Node {
 	return &Node{
-		Children:      make([]*Node, 0),
-		Parent:        parent,
-		score:         0,
-		currentPlayer: nextPlayer,
-		board:         board,
-		pawn:          pawn,
-		initialCords:  initialCords,
-		nestingLevel:  nestingLevel,
-		comparator:    comparator,
+		Children:     make([]*Node, 0),
+		score:        0,
+		pawn:         pawn,
+		initialCords: initialCords,
 	}
 }
 
 func (node *Node) String() string {
-	return fmt.Sprintf("%p : score: %d, kids: %d", node.Parent, node.score, len(node.Children))
+	return fmt.Sprintf("score: %d, kids: %d", node.score, len(node.Children))
 }
 
 func (node *Node) selectScore(parentChannel chan struct {
 	*Node
 	int8
-}, kidIdx int8) {
+}, kidIdx int8, board *Board, nestingLevel int8, currentPlayer utils.Player) {
 	//defer func() {
 	//	tab := ""
-	//	for i := int8(0); i < node.nestingLevel; i++ {
+	//	for i := int8(0); i < nestingLevel; i++ {
 	//		tab += "\t"
 	//	}
 	//	fmt.Println(tab, node.String())
 	//}()
-	if node.nestingLevel == utils.DEPTH {
-		node.score = DistanceScore(node.board, getOpponent(node.currentPlayer))
+	comparator := findMax
+	if isOpponent(nestingLevel) {
+		comparator = findMin
+	}
+
+	if nestingLevel == utils.DEPTH {
+		node.score = DistanceScore(board, getOpponent(currentPlayer))
 		parentChannel <- struct {
 			*Node
 			int8
 		}{node, kidIdx}
 		return
 	}
+	moves, pawns := getMovesAndPawns(board, &currentPlayer)
 
-	moves, pawns := getMovesAndPawns(node.board, &node.currentPlayer)
-	nextPlayer := getOpponent(node.currentPlayer)
+	channelForKids := make(chan struct {
+		*Node
+		int8
+	}, len(moves))
 
 	for i := range moves {
 		currentBoard := new(Board)
-		*currentBoard = *node.board
+		*currentBoard = *board
 
 		currentBoard.Pawns = [utils.PAWNS]*Pawn{}
 		for i := int8(0); i < utils.PAWNS; i++ {
 			currentBoard.Pawns[i] = new(Pawn)
-			*currentBoard.Pawns[i] = *node.board.Pawns[i]
+			*currentBoard.Pawns[i] = *board.Pawns[i]
 		}
 		var currentPawn *Pawn
 		for _, pawn := range currentBoard.Pawns {
@@ -111,31 +103,24 @@ func (node *Node) selectScore(parentChannel chan struct {
 		if !done {
 			continue
 		}
-		newNode := NewNode(node, currentBoard, node.nestingLevel+1, nextPlayer, currentPawn, initialPosition)
+		newNode := NewNode(currentPawn, initialPosition)
 		node.Children = append(node.Children, newNode)
-	}
-	//fmt.Println("Children: ", len(node.Children))
-	channelForKids := make(chan struct {
-		*Node
-		int8
-	}, len(node.Children))
-	if !isOpponent(node.nestingLevel) { // !isOpponent(node.nestingLevel)
-		for i, child := range node.Children {
-			go child.selectScore(channelForKids, int8(i))
-		}
-	} else {
-		for i, child := range node.Children {
-			child.selectScore(channelForKids, int8(i))
+
+		if !isOpponent(nestingLevel) { //
+			go newNode.selectScore(channelForKids, int8(i), currentBoard, nestingLevel+1, getOpponent(currentPlayer))
+		} else {
+			newNode.selectScore(channelForKids, int8(i), currentBoard, nestingLevel+1, getOpponent(currentPlayer))
 		}
 	}
+
 	node.score = 0
-	if isOpponent(node.nestingLevel) {
+	if isOpponent(nestingLevel) {
 		node.score = math.MaxInt
 	}
 	bestIdx := int8(-1)
 	for range node.Children {
 		kidNode := <-channelForKids
-		if node.comparator(kidNode.score, node.score) {
+		if comparator(kidNode.score, node.score) {
 			node.score = kidNode.score
 			bestIdx = kidNode.int8
 		}
